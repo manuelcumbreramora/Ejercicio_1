836f80c1aa09eb41186e7f8b972fefa1920ce007
manuelcumbreramora <58508423+manuelcumbreramora@users.noreply.github.com>
2020-02-06 11:42:27 +0100

    esto es una prueba

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Modificado de nuevo");
+            Console.WriteLine("Hello World! Modificado de nuevo otra vez");
         }
     }
 }
