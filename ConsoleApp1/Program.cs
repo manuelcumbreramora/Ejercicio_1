e9a3d10709131d75535d951d88382d0f6556e7cb
manuelcumbreramora <58508423+manuelcumbreramora@users.noreply.github.com>
2020-04-13 12:01:39 +0200

    af

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Modificado de nuevo otra vez");
+            Console.WriteLine("Hello World! Modificado de nuevo otra vez desde mi rama desarrollo ");
         }
     }
 }
