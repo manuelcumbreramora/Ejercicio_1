4d4dc5d70ee1484287b3e943c8e49c7beaa84ee7
ManuelAntonioRegueiraSomoza <62422574+ManuelAntonioRegueiraSomoza@users.noreply.github.com>
2020-04-13 16:56:15 +0200

    Inserto mi clase ManuelAntonio

---
+++ ConsoleApp1/ManuelAntonio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class ManuelAntonio
+    {
+        private string Nombre;
+        public ManuelAntonio()
+        {
+            Nombre = "Manuel Antonio";
+        }
+
+        public string GetNombre()
+        {
+            return this.Nombre;
+        }
+    }
+}
