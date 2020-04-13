035847ba0d90827a97b38f446032beba96b48be7
manuelcumbreramora <58508423+manuelcumbreramora@users.noreply.github.com>
2020-04-13 16:09:15 +0200

    situación inicial

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,8 +7,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World! nueva línea");
-            Console.WriteLine("Hello World! nueva línea 333333");
         }
     }
 }
