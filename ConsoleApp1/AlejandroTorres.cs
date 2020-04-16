0360c9d6c6f7b638305e05f1014011eda500bb43
AlejandroTD257 <AlejandroTorres257@gmail.com>
2020-04-16 10:24:50 +0200

    Name changed and new method
    Name changed from Alejandro to AlejandroTorres
    
    Added the method HolaAlejandro to AlejandroTorres class

---
+++ ConsoleApp1/AlejandroTorres.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class AlejandroTorres
+    {
+        public void HolaAlejandro()
+        {
+            global::System.Console.WriteLine("Hola, Alejandro");
+        }
+    }
+}
